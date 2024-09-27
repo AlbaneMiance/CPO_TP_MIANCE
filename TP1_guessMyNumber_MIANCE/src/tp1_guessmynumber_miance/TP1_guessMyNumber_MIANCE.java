7b6b908b9a43ac692f58115ea64c3d280f089c26
Albane Miance <albane.miance@epfedu.fr>
2024-09-27 16:50:41 +0200

    TP1 - Exo 3 - Guess my Number - in progress

--- TP1_guessMyNumber_MIANCE/src/tp1_guessmynumber_miance/TP1_guessMyNumber_MIANCE.java
+++ TP1_guessMyNumber_MIANCE/src/tp1_guessmynumber_miance/TP1_guessMyNumber_MIANCE.java
@@ -5,6 +5,7 @@
 package tp1_guessmynumber_miance;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -15,13 +16,33 @@ public class TP1_guessMyNumber_MIANCE {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {        // Création du générateur de nombres aléatoires
-        Random generateurAleat = new Random();
 
-        // Affichage de 5 nombres aléatoires inférieurs ou égaux à 100
-        for (int i = 0; i < 5; i++) {
-            int n = generateurAleat.nextInt(101); // 101 pour inclure 100
-            System.out.println("Nombre aléatoire " + (i + 1) + ": " + n);
+     public static void main(String[] args) {
+        // Création du générateur de nombres aléatoires
+        Random generateurAleat = new Random();
+        int nombreAleatoire = generateurAleat.nextInt(101); // Génère un nombre entre 0 et 100
+        Scanner sc = new Scanner(System.in);
+        int nombreDevine = -1; // Initialisation à une valeur qui ne peut pas être le nombre à deviner
+        int tentatives = 0; // Initialisation du compteur de tentatives
+
+        // Boucle pour demander à l'utilisateur de deviner le nombre
+        while (nombreDevine != nombreAleatoire) {
+            System.out.print("Devinez un nombre entre 0 et 100 : ");
+            nombreDevine = sc.nextInt();
+            tentatives++; // Incrémente le compteur à chaque tentative
+
+            // Comparaison du nombre deviné avec le nombre aléatoire
+            if (nombreDevine < nombreAleatoire) {
+                System.out.println("Trop petit !");
+            } else if (nombreDevine > nombreAleatoire) {
+                System.out.println("Trop grand !");
+            } else {
+                System.out.println("Gagné !");
+                System.out.println("Vous avez trouvé le nombre en " + tentatives + " tentatives.");
+            }
         }
+
+        // Fermeture du scanner
+        sc.close();
     }
-}   // TODO code application logic here
+}
