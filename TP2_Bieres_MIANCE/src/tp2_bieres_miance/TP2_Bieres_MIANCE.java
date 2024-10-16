3307bd3ba6c48a435665a3bf951759b1295b274b
Albane Miance <albane.miance@epfedu.fr>
2024-10-16 09:30:27 +0200

    TP2 - Etape 12 - In progress

--- TP2_Bieres_MIANCE/src/tp2_bieres_miance/TP2_Bieres_MIANCE.java
+++ TP2_Bieres_MIANCE/src/tp2_bieres_miance/TP2_Bieres_MIANCE.java
@@ -13,26 +13,27 @@ public class TP2_Bieres_MIANCE {
     /**
      * @param args the command line arguments
      */
-public static void main(String[] args) {
-    
+    public class main {
+    public static void main(String[] args) {
         // Création de la première bière : "Cuvée des trolls" de "Dubuisson" avec 7.0 degrés
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
         uneBiere.lireEtiquette();
-        System.out.println(); // Ligne vide pour séparer les étiquettes
+        uneBiere.Decapsuler();
 
         // Création de la deuxième bière : "Leffe" de "Abbaye de Leffe" avec 6.6 degrés
         BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         deuxBiere.lireEtiquette();
-        System.out.println();
+        deuxBiere.Decapsuler();
 
         // Création de la troisième bière : "Heineken" de "Heineken International" avec 5.0 degrés
         BouteilleBiere troisBiere = new BouteilleBiere("Heineken", 5.0, "Heineken International");
         troisBiere.lireEtiquette();
-        System.out.println();
+        troisBiere.Decapsuler();
 
         // Création de la quatrième bière : "Guinness" de "Guinness Brewery" avec 4.2 degrés
         BouteilleBiere quatreBiere = new BouteilleBiere("Guinness", 4.2, "Guinness Brewery");
         quatreBiere.lireEtiquette();
-        System.out.println();
+        quatreBiere.Decapsuler();
+    }
     }
 }
